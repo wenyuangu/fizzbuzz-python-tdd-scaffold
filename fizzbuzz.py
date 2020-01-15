dcd5613426d887cdb34775ce2ea2ee77f6d14049
guwenyuan <wg2306@columbia.edu>
2020-01-15 14:46:06 +0800

    fizzbuzz
    v2

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,15 @@
+# @File  : fizzbuzz.py
+# @Author: Wenyuan Gu
+# @Date  : 2020/1/15
+
+
 def fizzbuzz(n):
-    for i in range(1,n + 1):
-        print('fizz'[i % 3 * 4:] + 'buzz'[i % 5 * 4:] or i)
+    return 'Fizz'[n % 3 * 4:] + 'Buzz'[n % 5 * 4:] or n
+
+
+def main():
+    for i in range(1, 101):
+        print(fizzbuzz(i))
+
+if __name__ == '__main__':
+    main()
--- fizzbuzz_test.py
+++ fizzbuzz_test.py
@@ -1,8 +1,20 @@
+# @File  : fizzbuzz_test.py
+# @Author: Wenyuan Gu
+# @Date  : 2020/1/15
+
+
+import pytest
 from fizzbuzz import fizzbuzz
 
+class Test_fizzbuzz():
+    def test2(self):
+        assert fizzbuzz(2) == 2
+    def test3(self):
+        assert fizzbuzz(3) == 'Fizz'
+    def test5(self):
+        assert fizzbuzz(5) == 'Buzz'
+    def test15(self):
+        assert fizzbuzz(15) == 'FizzBuzz'
 
-def test_fizzbuzz():
-    fizzbuzz(100)
-
-if __name__ == '__main__':
-	test_fizzbuzz()
+if __name__ == "__main__":
+    pytest.main()
